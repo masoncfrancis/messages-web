bd2ab5141bb51edd2fd6c985ce3e77c11082845b
Mason Francis <a02244655@usu.edu>
2023-06-09 13:54:12 -0600

    Added login page and path

--- messagesapp/src/App.tsx
+++ messagesapp/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ messagesapp/src/pages/LoginPage.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-4 col-md-6 col-sm-8">
+          <div className="card mt-5">
+            <div className="card-body">
+              <h3 className="card-title text-center mb-4">Login</h3>
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">
+                    Email address
+                  </label>
+                  <input type="email" className="form-control" id="email" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input type="password" className="form-control" id="password" />
+                </div>
+                <div className="d-grid">
+                  <button type="submit" className="btn btn-primary">
+                    Log in
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
